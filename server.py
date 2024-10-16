08903f08579c7e29eebfeea7ac87f9cc2e44fdbd
Aspegic <souhail.smiriaa@gmail.com>
2024-10-16 16:04:32 +0100

    16/10

--- server.py
+++ server.py
@@ -1,11 +1,37 @@
+# Import required modules
 import socket
 import threading
 
+HOST = '127.0.0.1'
+PORT = 1234 # You can use any port between 0 to 65535
+LISTENER_LIMIT = 5
 
 
+# Main function
 def main():
-    pass
 
+    # Creating the socket class object
+    # AF_INET: we are going to use IPv4 addresses
+    # SOCK_STREAM: we are using TCP packets for communication
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Creating a try catch block
+    try:
+        # Provide the server with an address in the form of
+        # host IP and port
+        server.bind((HOST, PORT))
+        print(f"Running the server on {HOST} {PORT}")
+    except:
+        print(f"Unable to bind to host {HOST} and port {PORT}")
+
+    # Set server limit
+    server.listen(LISTENER_LIMIT)
+
+    # This while loop will keep listening to client connections
+    while 1:
+
+        client, address = server.accept()
+        print(f"Successfully connected to client {address[0]} {address[1]}")
 
 
 if __name__ == '__main__':
