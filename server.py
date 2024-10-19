567923354c9d747d6d1c79303a8b59539e2adac4
Aspegic <souhail.smiriaa@gmail.com>
2024-10-19 23:36:10 +0100

    19/10

--- server.py
+++ server.py
@@ -7,6 +7,26 @@ PORT = 1234 # You can use any port between 0 to 65535
 LISTENER_LIMIT = 5
 active_clients = [] # List of all currently connected users
 
+
+
+
+
+
+def listen_for_messages(client, username):
+
+    while 1:
+
+        message = client.recv(2048).decode('utf-8')
+        if message != '':
+            
+            final_msg = username + '~' + message
+            send_messages_to_all(final_msg)
+
+        else:
+            print(f"The message send from client {username} is empty")
+
+
+
 # Function to send message to a single client
 def send_message_to_client(client, message):
 
